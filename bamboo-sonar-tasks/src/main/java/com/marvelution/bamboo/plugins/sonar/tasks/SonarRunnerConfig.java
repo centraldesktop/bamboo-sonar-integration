e4398d76fec0899fed98ae7351369273a34e37b4
Mark Rekveld <markrekveld@marvelution.com>
2011-12-31 15:25:20 +0100

    Added the missing empty check for the custom extra parameters field for the Sonar Runner Config helper

--- bamboo-sonar-tasks/src/main/java/com/marvelution/bamboo/plugins/sonar/tasks/SonarRunnerConfig.java
+++ bamboo-sonar-tasks/src/main/java/com/marvelution/bamboo/plugins/sonar/tasks/SonarRunnerConfig.java
@@ -73,7 +73,9 @@ public class SonarRunnerConfig {
 		environmentVariables = ((String)taskContext.getConfigurationMap().get(CFG_ENVIRONMENT_VARIABLES));
 		workingDirectory = taskContext.getWorkingDirectory();
 		commandline.add(getSonarRunnerExecutable(builderPath));
-		commandline.add(taskContext.getConfigurationMap().get(CFG_SONAR_EXTRA_CUSTOM_PARAMETERS));
+		if (StringUtils.isNotBlank(taskContext.getConfigurationMap().get(CFG_SONAR_EXTRA_CUSTOM_PARAMETERS))) {
+			commandline.add(taskContext.getConfigurationMap().get(CFG_SONAR_EXTRA_CUSTOM_PARAMETERS));
+		}
 		environment.putAll(environmentVariableAccessor.splitEnvironmentAssignments(environmentVariables, false));
 		environment.put(SONAR_RUNNER_HOME, builderPath);
 	}
